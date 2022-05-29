6657823e15a5b024cdd1b19b0d266d616d137d2f
pepukrit <pepukrit@hotmail.com>
2022-05-29 16:21:14 +0700

    Initial Commit

---
+++ Shared/SwiftUIMVVMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMAppApp.swift
+//  Shared
+//
+//  Created by Ukrit Wattanakulchart on 29/5/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
